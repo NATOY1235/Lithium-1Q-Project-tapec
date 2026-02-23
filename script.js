efbc8f3ad2cf1bdd927eff91a6017ce72cc17e4c
NATOY1235 <arvsoberano29@src.pshs.edu.ph>
2026-02-23 08:14:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+let words = ["apple", "bread", "chair", "planet", "computer"];
+
+
+let currentWord = "";
+let score = 0;
+
+
+let scrambledWord = document.getElementById("scrambledWord");
+let userInput = document.getElementById("userInput");
+let result = document.getElementById("result");
+let scoreDisplay = document.getElementById("score");
+
+let startBtn = document.getElementById("startBtn");
+let submitBtn = document.getElementById("submitBtn");
+
+
+function shuffle(word) {
+    return word.split("").sort(() => Math.random() - 0.5).join("");
+}
+
+
+function startGame() {
+    score = 0;
+    scoreDisplay.textContent = score;
+    newWord();
+}
+
+
+function newWord() {
+    let randomIndex = Math.floor(Math.random() * words.length);
+    currentWord = words[randomIndex];
+    scrambledWord.textContent = shuffle(currentWord);
+    userInput.value = "";
+    result.textContent = "";
+}
+
+
+function checkAnswer() {
+    let answer = userInput.value.toLowerCase();
+
+    if (answer === currentWord) {
+        score++;
+        scoreDisplay.textContent = score;
+        result.textContent = "Correct!";
+    } else {
+        result.textContent = "Wrong! Try again.";
+    }
+}
+
+
+startBtn.addEventListener("click", startGame);
+submitBtn.addEventListener("click", checkAnswer);
